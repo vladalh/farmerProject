d91f3c2bd5d215c68e39d392e7dd2833c977ae53
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2022-02-05 22:59:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ user_name = input("Enter gardener's name ")
 
 if __name__ == '__main__':
     Gardener.knowledge_base()
-    user_tomato_bush = TomatoBush(4)
+    user_tomato_bush = TomatoBush(8)
     gardener = Gardener(user_name, user_tomato_bush)
     gardener.work()
     gardener.work()
