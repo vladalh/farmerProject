c1fa838440b6cf01030ff1d6325fc9a0c15fe2f0
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2022-02-05 23:00:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from tomato_bush import TomatoBush
 from gardener import Gardener
 
 user_name = input("Enter gardener's name ")
+number = int(input())
 
 if __name__ == '__main__':
     Gardener.knowledge_base()
-    user_tomato_bush = TomatoBush(8)
+    user_tomato_bush = TomatoBush(number)
     gardener = Gardener(user_name, user_tomato_bush)
     gardener.work()
     gardener.work()
